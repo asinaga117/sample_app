cac01205fc51faa81da99f0da5fed88eb95495bb
asinaga117 <asinaga1994@gmail.com>
2022-03-16 11:07:39 +0000

    バリデーションの追加

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,13 +5,12 @@ class ListsController < ApplicationController
   end
   
   def create
-    #1.&2. データを受け取り新規登録するためのインスタンス作成
-    list = List.new(list_params)
-    # 3. データをデータベースに保存するためのsaveメソッド実行
-    list.save
-    # redirect_to '/top' を削除して、以下コードに変更
-    # 詳細画面へリダイレクト
-    redirect_to list_path(list.id)
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list.id)
+    else
+      render :new
+    end
   end
 
   def index
@@ -37,7 +36,7 @@ class ListsController < ApplicationController
     list.destroy #データ(レコード)を削除
     redirect_to '/lists' #投稿一覧画面へリダイレクト
   end
-
+  
   
   private
   # ストロングパラメーター
